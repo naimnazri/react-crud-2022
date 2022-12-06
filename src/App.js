90454840cce8fff94e16904cfface7532ecdfa45
Naim <nazrinaim@gmail.com>
2022-12-06 12:55:44 +0800

    Add new file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes,Route,Navigate} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
+import EmployeeDataTable from './components/EmployeeDataTable';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div  class="container card mb-4 box-shadow">
+
+        <div class="card-header">
+            <h4 class="my-0 font-weight-normal">React Crud 2022</h4>
+          </div>
+
+    <Routes>
+        <Route path="/" element={<Navigate to="/read" />} />
+        <Route exact path="/create" element={<AddEmployee/>}/>
+        <Route exact path="/read" element={<EmployeeDataTable/>}/>
+        <Route path="/edit/:id" element={<EditEmployee/>}/>
+      </Routes>
+
     </div>
   );
 }
